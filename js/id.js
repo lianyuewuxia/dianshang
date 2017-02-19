e30c775eaf43b81cf99cd8d86f22ae5295eb69ab
lianyuewuxia <734928363@qq.com>
2017-02-19 22:14:50 +0800

    一个练习的项目
    未完成

---
+++ js/id.js
@@ -0,0 +1,9 @@
+/**
+ * Created by A-JUN on 2017/2/15.
+ */
+window.onload(){
+    var Odiv=document.getElementsByTagName("odiv1");
+    var Ddiv=document.getElementsBytagName("odiv2");
+
+    function
+}
